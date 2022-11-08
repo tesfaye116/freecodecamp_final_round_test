ba72b2f30492b86c590fa5c012773b88e1613b70
tesfa <tesfayegirma360@gmail.com>
2022-11-08 23:28:22 +0300

    freecodecamp update

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,6 +1,39 @@
 def arithmetic_arranger(problems):
-    for problem in problems:
-        x = problem
-        x = x.replace(" ", '\n', 1)
-        
-    return x
+    if len(problems) > 5:
+        return "Error: Too many problems."
+    else:
+        first_line = ""
+        second_line = ""
+        third_line = ""
+        result_line = ""
+        for problem in problems:
+
+            first_num, operator, second_num = problem.split()
+
+            if not first_num.isdigit() or not second_num.isdigit():
+                return "Error: Numbers must only contain digits."
+            if len(first_num) > 4 or len(second_num) > 4:
+                return "Error: Numbers cannot be more than four digits."
+            if operator == "+":
+                result = int(first_num) + int(second_num)
+            elif operator == "-":
+                result = int(first_num) - int(second_num)
+            else:
+                return "Error: Operator must be '+' or '-'."
+            first_num = str(first_num)
+            second_num = str(second_num)
+            result = str(result)
+            first_line += first_num.rjust(max(len(first_num),
+                                          len(second_num)) + 2) + "   "
+            second_line += operator + \
+                second_num.rjust(
+                    max(len(first_num), len(second_num)) + 1) + "   "
+            third_line += "-" * \
+                (max(len(first_num), len(second_num)) + 2) + "   "
+
+            result_line += result.rjust(
+                max(len(first_num), len(second_num)) + 2) + "   "
+
+        arranged_problems = first_line + "\n" + second_line + \
+            "\n" + third_line + "\n" + result_line
+        return arranged_problems
